f2e19dec819fcd8e273ace95e8662aec785cd43c
YasserDalali <yasser.dalali.personal@gmail.com>
2024-09-30 17:01:09 +0100

    Working face detection with 4 people

--- public/scripts.js
+++ public/scripts.js
@@ -1,76 +1,76 @@
-console.log(faceapi)
+const video = document.getElementById("video");
 
-const run = async()=>{
-    //loading the models is going to use await
-    const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: false,
+Promise.all([
+  faceapi.nets.ssdMobilenetv1.loadFromUri("models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("models"),
+]).then(startWebcam);
+
+function startWebcam() {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: false,
     })
-    const videoFeedEl = document.getElementById('video-feed')
-    videoFeedEl.srcObject = stream
-    //we need to load our models
-    // pre-trained machine learning for our facial detection!
-    await Promise.all([
-        faceapi.nets.ssdMobilenetv1.loadFromUri('./models'),
-        faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
-        faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
-        faceapi.nets.ageGenderNet.loadFromUri('./models'),
-        faceapi.nets.faceExpressionNet.loadFromUri('./models'),
-    ])
-
-    //make the canvas the same size and in the same location
-    // as our video feed
-    const canvas = document.getElementById('canvas')
-    canvas.style.left = videoFeedEl.offsetLeft
-    canvas.style.top = videoFeedEl.offsetTop
-    canvas.height = videoFeedEl.height
-    canvas.width = videoFeedEl.width
-
-    /////OUR FACIAL RECOGNITION DATA
-    // we KNOW who this is (Michael Jordan)
-    const refFace = await faceapi.fetchImage('https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Michael_Jordan_in_2014.jpg/220px-Michael_Jordan_in_2014.jpg')
-    //we grab the reference image, and hand it to detectAllFaces method
-    let refFaceAiData = await faceapi.detectAllFaces(refFace).withFaceLandmarks().withFaceDescriptors()
-    let faceMatcher = new faceapi.FaceMatcher(refFaceAiData)
-
-    // facial detection with points
-    setInterval(async()=>{
-        // get the video feed and hand it to detectAllFaces method
-        let faceAIData = await faceapi.detectAllFaces(videoFeedEl).withFaceLandmarks().withFaceDescriptors().withAgeAndGender().withFaceExpressions()
-        // console.log(faceAIData)
-        // we have a ton of good facial detection data in faceAIData
-        // faceAIData is an array, one element for each face
-
-        // draw on our face/canvas
-        //first, clear the canvas
-        canvas.getContext('2d').clearRect(0,0,canvas.width,canvas.height)
-        // draw our bounding box
-        faceAIData = faceapi.resizeResults(faceAIData,videoFeedEl)
-        faceapi.draw.drawDetections(canvas,faceAIData)
-        faceapi.draw.drawFaceLandmarks(canvas,faceAIData)
-        faceapi.draw.drawFaceExpressions(canvas,faceAIData)
-
-        // ask AI to guess age and gender with confidence level
-        faceAIData.forEach(face=>{
-            const { age, gender, genderProbability, detection, descriptor } = face
-            const genderText = `${gender} - ${Math.round(genderProbability*100)/100*100}`
-            const ageText = `${Math.round(age)} years`
-            const textField = new faceapi.draw.DrawTextField([genderText,ageText],face.detection.box.topRight)
-            textField.draw(canvas)
-
-            let label = faceMatcher.findBestMatch(descriptor).toString()
-            // console.log(label)
-            let options = {label: "Jordan"}
-            if(label.includes("unknown")){
-                options = {label: "Unknown subject..."}
-            }
-            const drawBox = new faceapi.draw.DrawBox(detection.box, options)
-            drawBox.draw(canvas)
-        })
-        
-
-    },200)
-
+    .then((stream) => {
+      video.srcObject = stream;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
-run()
+function getLabeledFaceDescriptions() {
+  const labels = ["Hamza", "Yasser", "Reda", "Zakaria"];
+  return Promise.all(
+    labels.map(async (label) => {
+      const descriptions = [];
+      for (let i = 1; i <= 1; i++) {
+        const img = await faceapi.fetchImage(`labels/${label}/${i}.jpg`);
+        const detections = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        descriptions.push(detections.descriptor);
+      }
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
+    })
+  );
+}
+
+
+video.addEventListener("play", async () => {
+  console.log("START")
+  const labeledFaceDescriptors = await getLabeledFaceDescriptions();
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
+
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  setInterval(async () => {
+    console.log("START2")
+
+    const detections = await faceapi
+      .detectAllFaces(video)
+      .withFaceLandmarks()
+      .withFaceDescriptors();
+
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+
+    const results = resizedDetections.map((d) => {
+      return faceMatcher.findBestMatch(d.descriptor);
+    });
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: result,
+      });
+      drawBox.draw(canvas)
+    });
+  }, 100);
+});
